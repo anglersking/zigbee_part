cd523a4fe6a3810515062c2b094f76c2de7905ac
wanshan.pang <wanshan.pang@momenta.ai>
2023-08-28 19:52:35 +0800

    update first

---
+++ Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿
+namespace Zigbee_part
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 25D);
+            this.Led_1_stadus = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Cheack = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Light_res = new System.Windows.Forms.Label();
+            this.lb_light = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.refrash = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Led_1_stadus
+            // 
+            this.Led_1_stadus.AutoSize = true;
+            this.Led_1_stadus.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Led_1_stadus.Location = new System.Drawing.Point(26, 36);
+            this.Led_1_stadus.Name = "Led_1_stadus";
+            this.Led_1_stadus.Size = new System.Drawing.Size(136, 20);
+            this.Led_1_stadus.TabIndex = 0;
+            this.Led_1_stadus.Text = "灯组1状态 ：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(168, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "关闭";
+            // 
+            // Cheack
+            // 
+            this.Cheack.AutoSize = true;
+            this.Cheack.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Cheack.Location = new System.Drawing.Point(26, 120);
+            this.Cheack.Name = "Cheack";
+            this.Cheack.Size = new System.Drawing.Size(125, 20);
+            this.Cheack.TabIndex = 2;
+            this.Cheack.Text = "红外探测 ：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(168, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "无人";
+            // 
+            // Light_res
+            // 
+            this.Light_res.AutoSize = true;
+            this.Light_res.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Light_res.Location = new System.Drawing.Point(26, 199);
+            this.Light_res.Name = "Light_res";
+            this.Light_res.Size = new System.Drawing.Size(125, 20);
+            this.Light_res.TabIndex = 4;
+            this.Light_res.Text = "光照强度 ：";
+            // 
+            // lb_light
+            // 
+            this.lb_light.AutoSize = true;
+            this.lb_light.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb_light.Location = new System.Drawing.Point(168, 199);
+            this.lb_light.Name = "lb_light";
+            this.lb_light.Size = new System.Drawing.Size(31, 20);
+            this.lb_light.TabIndex = 5;
+            this.lb_light.Text = "0%";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.Cheack);
+            this.groupBox1.Controls.Add(this.lb_light);
+            this.groupBox1.Controls.Add(this.Led_1_stadus);
+            this.groupBox1.Controls.Add(this.Light_res);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(337, 245);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "监测状态反馈区";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "开关";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.refrash);
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 292);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(766, 271);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "历史数据";
+            // 
+            // refrash
+            // 
+            this.refrash.Location = new System.Drawing.Point(6, 30);
+            this.refrash.Name = "refrash";
+            this.refrash.Size = new System.Drawing.Size(75, 23);
+            this.refrash.TabIndex = 1;
+            this.refrash.Text = "刷新";
+            this.refrash.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(754, 206);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.chart1);
+            this.groupBox3.Location = new System.Drawing.Point(384, 26);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(394, 245);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "实时波形";
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.CursorX.IsUserEnabled = true;
+            chartArea1.CursorX.IsUserSelectionEnabled = true;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(18, 36);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.MarkerColor = System.Drawing.Color.Red;
+            series1.MarkerSize = 10;
+            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series1.Name = "光照强度";
+            series1.Points.Add(dataPoint1);
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(370, 194);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(16, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "当前时间";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 575);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Zigbee_part";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Led_1_stadus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Cheack;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Light_res;
+        private System.Windows.Forms.Label lb_light;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button refrash;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,237 @@
+﻿using Aardvark.Base;
+using Segment.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zigbee_part
+{
+    public partial class Form1 : Form
+    {
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        System.Threading.Thread threadWatch = null; //负责监听客户端的线程
+        Socket socketWatch = null; //负责监听客户端的套接字
+                                   //创建一个负责和客户端通信的套接字 
+        List<Socket> socConnections = new List<Socket>();
+        List<Thread> dictThread = new List<Thread>();
+        Random r = new Random();
+        int i = 0;
+        double num = 0.0;
+        int stop = 0;
+
+
+
+        /// <summary>
+        /// 监听客户端发来的请求
+        /// </summary>
+        private void WatchConnecting()
+        {
+            while (true)  //持续不断监听客户端发来的请求
+            {
+                Socket socConnection = socketWatch.Accept();
+                //txtMsg.AppendText("客户端连接成功" + "\r\n");
+                //创建一个通信线程 
+                ParameterizedThreadStart pts = new ParameterizedThreadStart(ServerRecMsg);
+                Thread thr = new Thread(pts);
+                thr.IsBackground = true;
+                socConnections.Add(socConnection);
+                //启动线程
+                thr.Start(socConnection);
+                dictThread.Add(thr);
+            }
+        }
+        private void ServerRecMsg(object socketClientPara)
+        {
+            Socket socketServer = socketClientPara as Socket; //类型转换 objec->Socket
+            while (true)
+            {
+                //创建一个内存缓冲区 其大小为1024*1024字节  即1M
+                byte[] arrServerRecMsg = new byte[1024 * 1024];
+                try
+                {
+                    //将接收到的信息存入到内存缓冲区,并返回其字节数组的长度
+                    int length = socketServer.Receive(arrServerRecMsg);
+                    //将机器接受到的字节数组转换为人可以读懂的字符串
+                    string strSRecMsg = Encoding.UTF8.GetString(arrServerRecMsg, 0, length);
+                    Console.WriteLine(length);
+                    if (strSRecMsg.Length != 0)
+                    {
+                        //将发送的字符串信息附加到文本框txtMsg上   客户端IP 时间  消息
+
+                        //  txtMsg.AppendText(socketServer.RemoteEndPoint.ToString() + "客户端 " + GetCurrentTime() + "\r\n" + strSRecMsg + "\r\n");
+
+                        num = ((float)r.NextDouble());
+
+                        num = r.NextDouble();
+                        var dt = DateTime.Now;//ToString("yyyy/MM/dd HH:mm:ss.ffff");
+
+
+                        dt = dt.AddMilliseconds(1.0);
+
+
+                        chart1.Series[0].Points.AddXY(dt.ToString("yyyy/MM/dd HH:mm:ss:fff"), 25 + num);
+                        i++;
+
+                        chart1.Series[0].BorderWidth = 5;
+                        chart1.Series[0].Color = Color.Blue;
+                        chart1.ChartAreas[0].AxisX.Title = "Time/ms";
+                        chart1.ChartAreas[0].AxisY.Title = "光强/%";
+
+                        lb_light.Text = String.Format("{0:F}", (25 + strSRecMsg[8]+ strSRecMsg[9])) + "%";
+                         if ((float)(strSRecMsg[1]+ strSRecMsg[2]) <= 5)
+                         {
+                             label1.Text = "有人";
+
+
+                         }
+                         else 
+                         {
+                             label1.Text = "无人";
+
+
+                         }
+
+                        this.dataGridView1.Rows.Insert(0, dt.ToString("yyyy/MM/dd HH:mm:ss:fff"), lb_light.Text, label1.Text, label2.Text);
+
+                        System.Console.WriteLine(strSRecMsg);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("错误：" + ex.ToString());
+                   this.label1.Text=socketServer.RemoteEndPoint.ToString() + "客户端已断开连接！" + "\r\n";
+
+                    // 从 通信套接字 集合中删除被中断连接的通信套接字；
+                  //  Dict.Remove(socketServer.RemoteEndPoint.ToString());
+                    // 从通信线程集合中删除被中断连接的通信线程对象；
+                //    dictThread.Remove(socketServer.RemoteEndPoint.ToString());
+                    // 从列表中移除被中断的连接IP
+          //lb_ipOnline.Items.Remove(socketServer.RemoteEndPoint.ToString());
+
+                    break;
+                }
+            }
+        }
+
+
+
+        private void btnSendMsg_Click(object sender, EventArgs e)
+        {
+            //调用 ServerSendMsg方法  发送信息到客户端
+           // ServerSendMsg(txtSendMsg.Text.Trim());
+        }
+
+        /// <summary>
+        /// 发送信息到客户端的方法
+        /// </summary>
+        /// <param name="sendMsg">发送的字符串信息</param>
+        private void ServerSendMsg(string sendMsg)
+        {
+            //将输入的字符串转换成 机器可以识别的字节数组
+            byte[] arrSendMsg = Encoding.UTF8.GetBytes(sendMsg);
+            //向客户端发送字节数组信息
+            foreach (Socket socConnection in socConnections)
+            {
+                socConnection.Send(arrSendMsg);
+            }
+
+            //将发送的字符串信息附加到文本框txtMsg上
+           // txtMsg.AppendText("So-flash:" + GetCurrentTime() + "\r\n" + sendMsg + "\r\n");
+            //}
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+           int i=0
+
+
+
+
+
+            if (i == 2000)
+            {
+                timer1.Stop();
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dataGridView1.Columns[0].HeaderText = "时间";
+            dataGridView1.Columns[1].HeaderText = "光照强度";
+            dataGridView1.Columns[2].HeaderText = "是否有人";
+            dataGridView1.Columns[3].HeaderText = "灯是否打开";
+            // this.dataGridView1.Columns.Add("0","时间", "光照强度", "是否有人", "灯组状态");
+            // this.dataGridView1.Rows.Add("时间", "光照强度", "是否有人","灯组状态");
+            //定义一个套接字用于监听客户端发来的信息  包含3个参数(IP4寻址协议,流式连接,TCP协议)
+            socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //服务端发送信息 需要1个IP地址和端口号
+            IPAddress ipaddress = IPAddress.Parse("127.0.0.1"); //获取文本框输入的IP地址
+            //将IP地址和端口号绑定到网络节点endpoint上 
+            IPEndPoint endpoint = new IPEndPoint(ipaddress, int.Parse("12345")); //获取文本框上输入的端口号
+            //监听绑定的网络节点
+            socketWatch.Bind(endpoint);
+            //将套接字的监听队列长度限制为20
+            socketWatch.Listen(20);
+            //创建一个监听线程 
+            threadWatch = new Thread(WatchConnecting);
+            //将窗体线程设置为与后台同步
+            threadWatch.IsBackground = true;
+            //启动线程
+            threadWatch.Start();
+            //启动线程后 txtMsg文本框显示相应提示
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (label2.Text == "关闭")
+            {
+                label2.Text = "开启";
+                ServerSendMsg("open");
+
+            }
+            else
+            {
+                label2.Text = "关闭";
+                ServerSendMsg("off");
+            }
+        }
+    }
+}
